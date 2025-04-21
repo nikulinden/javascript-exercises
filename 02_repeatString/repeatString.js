f653f79209f358df3d2130732f1f0b6065964636
Denis Nikulin <nikulinden@gmail.com>
2025-04-20 21:07:52 -0700

    initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, repetitions) {
+    let temp = '';
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+    
+    for (let i = 0; i < repetitions; i++) {
+        temp += stringToRepeat;
+    }
+    return temp;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input_string) {
+    let charArray = input_string.split("");
+    let reversedArray = [];
+    for (let char = charArray.length-1; char >= 0; char--){
+        reversedArray.push(charArray[char])
+    }
+    let finalString = reversedArray.join("");
+    return finalString;
 };
 
 // Do not edit below this line
