e3eb74c320f6915f8573a3b69df748a4b37e090f
Denis Nikulin <nikulinden@gmail.com>
2025-04-20 21:07:29 -0700

    initial commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, toRemove) {
+    for (let i = 0; i < originalArray.length; i++) {
+        if (originalArray[i] === toRemove) {
+            originalArray.splice(i, 1);
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
